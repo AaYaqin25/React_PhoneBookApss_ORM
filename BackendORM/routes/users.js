cb228d0a67e300f56fe2e8d1faf33429be66308d
AaYaqin25 <ahmadainulyaqin777@gmail.com>
2022-12-15 18:20:53 +0700

    API Done

--- BackendORM/routes/users.js
+++ BackendORM/routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/index.js')
+var { Response } = require('../helpers/util.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async function (req, res, next) {
+  try {
+    const getUser = await models.User.findAll()
+    res.json(new Response(getUser))
+  } catch (error) {
+    res.status(500).json(new Response(error, false))
+  }
+});
+
+router.post('/', async function (req, res, next) {
+  try {
+    const addUser = await models.User.create(req.body)
+    res.json(new Response(addUser))
+  } catch (error) {
+    res.status(500).json(new Response(error, false))
+  }
+});
+
+router.put('/:id', async function (req, res, next) {
+  try {
+    const updateUser = await models.User.update({
+      name: req.body.name,
+      phone: req.body.phone
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(new Response(updateUser))
+  } catch (error) {
+    res.status(500).json(new Response(error, false))
+  }
+});
+
+router.delete('/:id', async function (req, res, next) {
+  try {
+    const deleteUser = await models.User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(new Response(deleteUser))
+  } catch (error) {
+    res.status(500).json(new Response(error, false))
+  }
 });
 
 module.exports = router;
